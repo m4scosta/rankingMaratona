a13fc97618d4e524b669f241802d86b9e54fb769
Marcos Costa <marcos@gtacsolutions.com>
2015-03-31 13:52:40 -0300

    corrigido settings.py

--- crawler_uri_fatec/settings.py
+++ crawler_uri_fatec/settings.py
@@ -56,6 +56,17 @@ ROOT_URLCONF = 'crawler_uri_fatec.urls'
 
 WSGI_APPLICATION = 'crawler_uri_fatec.wsgi.application'
 
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
